6e4c32134e8c0d1119a40ae0f623483cdf6225aa
ladinar <ladinarnanda@gmail.com>
2018-06-06 09:46:59 -0700

    initial commit

---
+++ resources/views/direktur/direktur.blade.php
@@ -0,0 +1,14 @@
+@extends('template.template')
+@section('content')
+<div class="content-wrapper">
+    <div class="container-fluid">
+      <!-- Breadcrumbs-->
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+          <a href="#">Direktur</a>
+        </li><!-- 
+        <li class="breadcrumb-item active">Direktur</li> -->
+      </ol>
+  </div>
+</div>
+@endsection

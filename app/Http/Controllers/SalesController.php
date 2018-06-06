3960285014ba1c38bfa98ceb948156aaec9fd7ff
Ladinar <ladinarnanda@gmail.com>
2018-06-05 23:21:28 -0700

    update view dashboard

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -13,7 +13,7 @@ class SalesController extends Controller
      */
     public function index()
     {
-        return view('direktur/direktur');
+        return view('dashboard/dashboard');
     }
 
     /**
